5618003a970f82f79b54a0615af01b6ca81c6ba8
Akshay <itsakshay2020@gmail.com>
2021-12-21 22:31:57 +0530

    Second round

--- src/test/java/Edge.java
+++ src/test/java/Edge.java
@@ -1,15 +1,15 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Edge {
 	@Test
 	void Edge()
 	{
-		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\drivers\\msedgedriver.exe");
-		WebDriver driver=new EdgeDriver();
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.get("https://google.com");
